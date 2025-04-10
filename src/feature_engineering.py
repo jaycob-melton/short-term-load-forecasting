de567f69ca55e2c603fcb499ce4144297cd935f2
jaycob-melton <jaycobmelton@berkeley.edu>
2025-04-10 16:01:40 -0700

    lstm

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import holidays
 
 def feature_engineering(df):
   # Time features
@@ -15,7 +16,13 @@ def feature_engineering(df):
   df["month_cos"] = np.cos(2 * np.pi * df["month"] / 12)
   # df["is_weekend"] = df["dayofweek"].isin([5, 6]).astype(int)
   # df = pd.get_dummies(df, columns=["dayofweek", "month"], drop_first=True).astype(int)
-  
+
+  df['date_only'] = df.index.date
+  #print(df['date_only'])
+  years = df.index.year.unique()
+  min_year, max_year = years.min(), years.max()
+  de_holidays = holidays.country_holidays("DE", years=range(min_year, max_year+1))
+  df['is_holiday'] = df['date_only'].isin(de_holidays).astype(int)
   # Lag features
   df["load_t-1"] = df["load"].shift(1)
   df["load_t-2"] = df["load"].shift(2)
@@ -40,10 +47,57 @@ def feature_engineering(df):
   # Drop rows with NA values (from shift/roll)
   df = df.dropna()
   y = df['load']
-  df = df.drop(columns=['load', 'load_forecast', 'solar', 'solar_profile', 
+  df = df.drop(columns=['date_only', 'load', 'load_forecast', 'solar', 'solar_profile', 
                         'wind', 'wind_profile', 'temp', 'rad_direct',
                         'rad_diffuse', "dayofweek", "month"])
   features = df.columns
   time_index = df.index
   return df.to_numpy(), y.to_numpy(), features.tolist(), time_index
 
+def feature_engineering_lstm(df):
+    # Time features
+  df["hour"] = df.index.hour
+  df["dayofweek"] = df.index.dayofweek
+  df["month"] = df.index.month
+  #df["hour_sin"] = np.sin(2 * np.pi * df["hour"] / 24)
+  #df["hour_cos"] = np.cos(2 * np.pi * df["hour"] / 24)
+  #df = pd.get_dummies(df, columns=["hour"], drop_first=True).astype(int)
+  df["hour_sin"] = np.sin(2 * np.pi * df["hour"] / 24)
+  df["hour_cos"] = np.cos(2 * np.pi * df["hour"] / 24)
+  df["dow_sin"] = np.sin(2 * np.pi * df["dayofweek"] / 7)
+  df["dow_cos"] = np.cos(2 * np.pi * df["dayofweek"] / 7)
+  df["month_sin"] = np.sin(2 * np.pi * df["month"] / 12)
+  df["month_cos"] = np.cos(2 * np.pi * df["month"] / 12)
+  # df["is_weekend"] = df["dayofweek"].isin([5, 6]).astype(int)
+  # df = pd.get_dummies(df, columns=["dayofweek", "month"], drop_first=True).astype(int)
+  
+
+  df['date_only'] = df.index.date
+  #print(df['date_only'])
+  years = df.index.year.unique()
+  min_year, max_year = years.min(), years.max()
+  de_holidays = holidays.country_holidays("DE", years=range(min_year, max_year+1))
+  df['is_holiday'] = df['date_only'].isin(de_holidays).astype(int)
+  
+  df["load-1"] = df["load"].shift(1)
+  # lag weather since we technically can't use weather at t to predict t
+  df["temp_t-1"] = df["temp"].shift(1)
+  # df["temp_t-6"] = df["temp"].shift(6)
+  
+  df["rad_direct_t-1"] = df["rad_direct"].shift(1)
+  # df["rad_direct_t-6"] = df["rad_direct"].shift(6)
+  
+  df["rad_diffuse_t-1"] = df["rad_diffuse"].shift(1)
+  # df["rad_diffuse_t-6"] = df["rad_diffuse"].shift(6)
+
+  
+
+  # Drop rows with NA values (from shift/roll)
+  df = df.dropna()
+  y = df['load']
+  df = df.drop(columns=['date_only', 'load', 'load_forecast', 'solar', 'solar_profile', 
+                        'wind', 'wind_profile', 'temp', 'rad_direct',
+                        'rad_diffuse', "dayofweek", "month"])
+  features = df.columns
+  time_index = df.index
+  return df.to_numpy(), y.to_numpy(), features.tolist(), time_index
---
+++ src/model.py
@@ -0,0 +1,122 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
+
+class LoadDataSet(Dataset):
+    def __init__(self, X, y, seq_len):
+        self.X = X
+        self.y = y
+        self.seq_len = seq_len
+
+    def __len__(self):
+        return len(self.X) - self.seq_len
+
+    def __getitem__(self, index):
+        x = self.X[index : index + self.seq_len]
+        y = self.y[index + self.seq_len]
+
+        return torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)
+
+
+
+class LSTMForecast(nn.Module):
+    def __init__(self, input_size=1, hidden_size=50, num_layers=2, output_size=1):
+        """
+        Args:
+            input_size (int): Number of features in the input.
+            hidden_size (int): Number of features in the hidden state.
+            num_layers (int): Number of recurrent layers.
+            output_size (int): Dimension of the output.
+        """
+        super(LSTMForecast, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+
+        # LSTM layer
+        self.lstm = nn.LSTM(
+            input_size=input_size, 
+            hidden_size=hidden_size, 
+            num_layers=num_layers, 
+            batch_first=True
+        )
+        # output layer
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        
+        out, _ = self.lstm(x, (h0, c0))
+        out = out[:, -1, :]
+        out = self.fc(out)
+        return out.squeeze()
+
+
+
+def train_lstm(model, train_loader, optimizer_params, num_epochs):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(device)
+    model.to(device)
+
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), **optimizer_params)
+
+    for epoch in range(num_epochs):
+        model.train()
+        train_loss = 0.0
+
+        for inputs, targets in train_loader:
+            inputs = inputs.to(device)
+            targets = targets.to(device)
+            
+            # inputs = inputs.unsqueeze(-1)
+
+            outputs = model(inputs)
+            targets = targets.squeeze()
+            loss = criterion(outputs, targets)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item() * inputs.size(0)
+            
+    train_loss /= len(train_loader.dataset)
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {train_loss:.4f}')
+
+
+def test_lstm(model, test_loader, scaler):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    model.eval()
+    predictions = []
+    actuals = []
+
+    with torch.no_grad():
+        for inputs, targets in test_loader:
+            inputs = inputs.to(device)
+            # inputs = inputs.unsqueeze(-1)
+            outputs = model(inputs)
+            predictions.append(outputs.cpu().numpy())
+            actuals.append(targets.cpu().numpy())
+
+    predictions = np.concatenate(predictions)
+    actuals = np.concatenate(actuals)
+
+    predictions_inv = scaler.inverse_transform(predictions.reshape(-1, 1))
+    actuals_inv = scaler.inverse_transform(actuals.reshape(-1, 1))
+
+    rmse = mean_squared_error(actuals_inv, predictions_inv, squared=False)
+    mae = mean_absolute_error(actuals_inv, predictions_inv)
+    mape = mean_absolute_percentage_error(actuals_inv, predictions_inv)
+
+    print(f"RMSE: {rmse:.2f}")
+    print(f"MAE: {mae:.2f}")
+    print(f"MAPE: {mape:.2f}%")
+
+    return predictions_inv, actuals_inv, {"RMSE": rmse, "MAE": mae, "MAPE": mape}
